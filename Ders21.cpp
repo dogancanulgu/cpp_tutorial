2d8d4764df2a11aaa98f8a25ad389df54531af83
Doğancan Ülgü <dogancan.ulgu@alo-tech.com>
2022-06-05 10:01:39 +0300

    Lecture 21

---
+++ Ders21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// void degerDegistir(int &ref) {
+
+//     ref = 20;
+// }
+
+int main() {
+    int a = 10;
+
+    // int &ref = a;
+    // ref++;
+
+    // int *ptr;
+    // ptr = &a;
+
+    // int &ref;
+    // ref = a;
+
+    cout << "a nin degeri: " << a << endl;
+
+    // degerDegistir(a);
+    // cout << "a nin yeni degeri: " << a << endl;
+
+    return 0;
+}
