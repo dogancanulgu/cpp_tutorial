3ef079c638519d9ef42580e8e32b40f742e3fbc0
Doğancan Ülgü <dogancan.ulgu@alo-tech.com>
2022-06-05 10:47:40 +0300

    Lecture 23

---
+++ Ders23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // const int i = 32;
+    // i = 40;
+    // cout << i << endl;
+
+    const int a[] = {10, 20, 30, 40};
+    // a[2] = 100; // error
+
+    return 0;
+}
