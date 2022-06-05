0ed7aada80974596c3c70743e2b7ebbbe57beb03
Doğancan Ülgü <dogancan.ulgu@alo-tech.com>
2022-06-05 09:57:15 +0300

    Lecture 20

---
+++ Ders20.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // int array[] = {1,2,3,4,};
+
+    // cout << array << endl;
+    // // cout << array + 1 << endl;
+    // // cout << array + 2 << endl;
+
+    // int *ptr = &array[2];
+    // cout << ptr << endl;
+    // // cout << ptr + 1 << endl;
+
+    string array[] = {"Dogancan", "Ulgu"};
+
+    // string *ptr = array;
+    // cout << ptr + 1 << endl;  
+    // cout << *(ptr + 1) << endl;
+    // cout << ptr[1] << endl; 
+
+    cout << array[3] << endl;
+
+    return 0;
+}
