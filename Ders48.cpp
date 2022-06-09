4d0fadf41c728815f2ea135b0dc8a7d0caa626c2
Doğancan Ülgü <dogancanulgu@gmail.com>
2022-06-09 14:14:19 +0300

    Lecture 48

---
+++ Ders48.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void Test() {
+    static int i = 3;
+    i++;
+    cout << "i'nin degeri: " << i << endl;
+}
+int main() {
+    Test();
+    Test();
+    Test();
+
+
+    return 0;
+}
