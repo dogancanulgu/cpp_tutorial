58b3f06961edeb11f833300de3f9511fda1d57ec
Doğancan Ülgü <dogancanulgu@gmail.com>
2022-06-09 14:14:32 +0300

    Lecture 49

---
+++ Ders49.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Gamer {
+    public:
+        static int gamers;
+        Gamer() {
+            gamers++;
+            cout << "Yeni Oyuncu Olusturuldu..." << endl;
+        }
+};
+
+int Gamer::gamers = 0;
+
+int main() {
+    Gamer gamer1;
+    Gamer gamer2;
+    Gamer gamer3;
+    Gamer gamer4;
+    cout << Gamer::gamers << endl;
+    return 0;
+}
