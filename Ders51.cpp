3fe91d7040efc000d2d06b72765a898e574f25f7
Doğancan Ülgü <dogancanulgu@gmail.com>
2022-06-09 14:14:57 +0300

    Lecture 51

---
+++ Ders51.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+    private:
+        string name;
+        int age;
+        int salary;
+    public:
+        Employee(string name, int age, int salary) {
+            this->name = name;
+            this->age = age;
+            this->salary = salary;
+        }
+        // friend void showInfos(Employee employee);
+        friend class Test;
+};
+
+class Test {
+    public:
+        static void showInfos(Employee employee) {
+            cout << employee.name << " " << employee.age << " " << employee.salary << endl;
+        }
+};
+
+// void showInfos(Employee employee) {
+//     cout << employee.name << " " << employee.age << " " << employee.salary << endl;
+// }
+
+int main() {
+    Employee employee("Dogancan", 35, 4000);
+    // showInfos(employee);
+    Test::showInfos(employee);
+    return 0;
+}
